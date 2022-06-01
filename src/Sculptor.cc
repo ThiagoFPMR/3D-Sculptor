0c8a5cb4bc53f9c8c2ea41075c7fa9f2cc6f0fa8
ThiagoFPMR <thiago.rodrigues.056@ufrn.edu.br>
2022-06-01 05:00:37 -0300

    writeOFF V1

--- src/Sculptor.cc
+++ src/Sculptor.cc
@@ -75,7 +75,7 @@ void Sculptor::putBox(int x0, int x1, int y0, int y1, int z0, int z1) {
     * in the box defined by the parameter values */
    for (int i = x0; i <= x1; i++) 
       for (int j = y0; j <= y1; j++) 
-         for (int k = z0; z <= z1; z++) {
+         for (int k = z0; k <= z1; k++) {
             // Activating the voxel
             v[i][j][k].isOn = true;
             // Setting the color of the voxel
@@ -96,7 +96,7 @@ void Sculptor::cutBox(int x0, int x1, int y0, int y1, int z0, int z1) {
    // Deactivating every voxel in the box defined by the parameter values
    for (int i = x0; i <= x1; i++) 
       for (int j = y0; j <= y1; j++) 
-         for (int k = z0; z <= z1; z++) {
+         for (int k = z0; k <= z1; k++) {
             // Deactivating the voxel
             v[i][j][k].isOn = false;          
          }
@@ -107,6 +107,7 @@ void Sculptor::putSphere(int xcenter, int ycenter, int zcenter, int radius) {
    /* For each voxel in the Sculptor object matrix, this method
     * checks whether they fit the equation of a sphere based
     * on their (i, j, k) coordinates and the parameters given */
+   int r2;
    // Checking if the parameters are valid
    if (xcenter >= nx || ycenter >= ny || zcenter >= nz)
       throw std::invalid_argument("Sculptor::putSphere(): arguments must be smaller than the respective matrix dimensions.");
@@ -114,9 +115,9 @@ void Sculptor::putSphere(int xcenter, int ycenter, int zcenter, int radius) {
     * in the sphere defined by the parameter values */ 
    for (int i = 0; i < nx; i++) 
       for (int j = 0; j < ny; j++) 
-         for (int k = 0; z < nz; z++) {
+         for (int k = 0; k < nz; k++) {
             // Defining the left side of the equation
-            r2 = pow(i - xcenter, 2) + pow(j - ycenter, 2) + pow(k - zcenter, 2)
+            r2 = pow(i - xcenter, 2) + pow(j - ycenter, 2) + pow(k - zcenter, 2);
             // Checking whether the equation is valid for the chosen voxel
             if (r2 == pow(radius, 2)) {
                // Activating the voxel
@@ -142,9 +143,9 @@ void Sculptor::cutSphere(int xcenter, int ycenter, int zcenter, int radius) {
    // Deactivating every voxel in the sphere defined by the parameter values
    for (int i = 0; i < nx; i++) 
       for (int j = 0; j < ny; j++) 
-         for (int k = 0; z < nz; z++) {
+         for (int k = 0; k < nz; k++) {
             // Defining the left side of the equation
-            r2 = pow(i - xcenter, 2) + pow(j - ycenter, 2) + pow(k - zcenter, 2)
+            r2 = pow(i - xcenter, 2) + pow(j - ycenter, 2) + pow(k - zcenter, 2);
             // Checking whether the equation is valid for the chosen voxel
             if (r2 == pow(radius, 2)) {
                // Deactivating the voxel
@@ -166,11 +167,11 @@ void Sculptor::putEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int r
     * in the ellipsoid defined by the parameter values */ 
    for (int i = 0; i < nx; i++) 
       for (int j = 0; j < ny; j++) 
-         for (int k = 0; z < nz; z++) {
+         for (int k = 0; k < nz; k++) {
             // Defining the left side of the equation
-            leftEq += (float)pow(i - xcenter, 2)/(float)pow(rx, 2)
-            leftEq += (float)pow(j - ycenter, 2)/(float)pow(ry, 2)
-            leftEq += (float)pow(k - zcenter, 2)/(float)pow(rz, 2)           
+            leftEq += (float)pow(i - xcenter, 2)/(float)pow(rx, 2);
+            leftEq += (float)pow(j - ycenter, 2)/(float)pow(ry, 2);
+            leftEq += (float)pow(k - zcenter, 2)/(float)pow(rz, 2);           
             // Checking whether the equation is valid for the chosen voxel
             if (leftEq == 1) {
                // Activating the voxel
@@ -196,15 +197,118 @@ void Sculptor::cutEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int r
    // Deactivating every voxel in the sphere defined by the parameter values
    for (int i = 0; i < nx; i++) 
       for (int j = 0; j < ny; j++) 
-         for (int k = 0; z < nz; z++) {
+         for (int k = 0; k < nz; k++) {
             // Defining the left side of the equation
-            leftEq += (float)pow(i - xcenter, 2)/(float)pow(rx, 2)
-            leftEq += (float)pow(j - ycenter, 2)/(float)pow(ry, 2)
-            leftEq += (float)pow(k - zcenter, 2)/(float)pow(rz, 2)           
+            leftEq += (float)pow(i - xcenter, 2)/(float)pow(rx, 2);
+            leftEq += (float)pow(j - ycenter, 2)/(float)pow(ry, 2);
+            leftEq += (float)pow(k - zcenter, 2)/(float)pow(rz, 2);           
             // Checking whether the equation is valid for the chosen voxel
             if (leftEq == 1) {
                // Deactivating the voxel
                v[i][j][k].isOn = false;
             }
          }
+}
+
+// Write .OFF File (TO-DO: Fix the mess that is its current state)
+void Sculptor::writeOFF(const char* filename) {
+   int Nfaces = 0, Nvertices = 0, **vertices, ***M;
+   float **faces;
+
+   faces = new float*[6*nx*ny*nz];
+   faces[0] = new float[6*nx*ny*nz*9]; 
+   
+   vertices = new int*[8*nx*ny*nz];
+   vertices[0] = new int[8*nx*ny*nz*3];
+
+   for (int i = 1; i < 6*nx*ny*nz; i++) faces[i] = faces[i-1] + 9; 
+   for (int i = 1; i < 8*nx*ny*nz; i++) vertices[i] = vertices[i-1] + 3; 
+
+   // Mapping matrix (not the most efficient approach but works)
+   M = new int**[(nx+1)];
+   M[0] = new int*[(nx+1)*(ny+1)];
+   M[0][0] = new int[(nx+1)*(ny+1)*(nz+1)];
+   // Making the auxiliary vetors point ot the elements vector
+   for (int i = 1; i < (nx+1); i++) M[i] = M[i-1] + ny; 
+   for (int i = 1; i < (nx+1)*(ny+1); i++) M[0][i] = M[0][i-1] + nz;
+   
+   for (int i = 0; i < nx+1; i++) 
+      for (int j = 0; j < ny+1; j++) 
+         for (int k = 0; k < nz+1; k++) 
+            M[i][j][k] = -1;
+
+   for (int i = 0; i < nx; i++) 
+      for (int j = 0; j < ny; j++) 
+         for (int k = 0; k < nz; k++) {
+               if (v[i][j][k].isOn) {
+                  //
+                  int faceCoords[6][4][3] = {
+                    {{i, j, k},   {i, j+1, k},   {i+1, j+1, k},   {i+1, j, k}}, // TOP
+                    {{i, j, k+1}, {i+1, j, k+1}, {i+1, j+1, k+1}, {i, j+1, k+1}}, // BOTTOM
+                    {{i, j, k},   {i+1, j, k},   {i+1, j, k+1},   {i, j, k+1}}, // LEFT
+                    {{i, j+1, k}, {i, j+1, k+1}, {i+1, j+1, k+1}, {i+1, j+1, k}}, // RIGHT
+                    {{i, j, k},   {i, j, k+1},   {i, j+1, k+1},   {i, j+1, k}}, // FRONT
+                    {{i+1, j, k}, {i+1, j+1, k}, {i+1, j+1, k+1}, {i+1, j, k+1}}  // BACK
+                  };
+                  for (int s = 0; s < 6; s++) {
+                     bool newFace = false;
+                     for (int n = 0; n < 4; n++) {
+                        if (M[faceCoords[s][n][0]][faceCoords[s][n][1]][faceCoords[s][n][2]] == -1) {
+                           vertices[Nvertices][0] = faceCoords[s][n][0];
+                           vertices[Nvertices][1] = faceCoords[s][n][1];
+                           vertices[Nvertices][2] = faceCoords[s][n][2];
+                           M[faceCoords[s][n][0]][faceCoords[s][n][1]][faceCoords[s][n][2]] = Nvertices;
+                           Nvertices++;
+                           newFace = true;  
+                        }
+                     }
+                     if (newFace) {
+                        faces[Nfaces][0] = 4;
+                        for (int n = 1; n <= 4; n++) 
+                           faces[Nfaces][n] = M[faceCoords[s][n][0]][faceCoords[s][n][1]][faceCoords[s][n][2]];
+                        faces[Nfaces][5] = v[i][j][k].r;
+                        faces[Nfaces][6] = v[i][j][k].g;
+                        faces[Nfaces][7] = v[i][j][k].b;
+                        faces[Nfaces][8] = v[i][j][k].a;
+                        Nfaces++;
+                     }
+                  }
+               }
+            }
+   
+
+   std::ofstream fout;
+   fout.open(filename);
+   if(!fout.is_open())
+      exit(1);
+   
+   fout << "OFF\n";
+   fout << Nvertices << " " << Nfaces << " 0\n";
+   for (int i = 0; i < Nvertices; i++) {
+      fout << vertices[i][0] << " ";
+      fout << vertices[i][1] << " ";
+      fout << vertices[i][2] << "\n";
+   }
+   for (int i = 0; i < Nfaces; i++) {
+      fout << faces[i][0] << " ";
+      fout << faces[i][1] << " ";
+      fout << faces[i][2] << " ";
+      fout << faces[i][3] << " ";
+      fout << faces[i][4] << " ";
+      fout << faces[i][5] << " ";
+      fout << faces[i][6] << " ";
+      fout << faces[i][7] << " ";
+      fout << faces[i][8] << "\n";
+   }
+   fout.close();
+
+   delete [] faces[0];
+   delete [] faces;
+   delete [] vertices[0];
+   delete [] vertices;
+   delete [] M[0][0];
+   delete [] M[0];
+   delete [] M;
+
+
 }
--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,19 @@
 #include <iostream>
 
 #include "../include/Voxel.h"
+#include "../include/Sculptor.h"
 
 
 int main() {
-    Voxel test;
-    test.a = 0.5;
-    std::cout << test.a << std::endl;
+    // cria um escultor cuja matriz tem 10x10x10 voxels
+    Sculptor trono(10,10,10);
+    // para mudar a cor do voxel
+    trono.setColor(0,0,1.0,1.0); // azul
+    // ativa os voxels na faixa de [x,y,z] pertencendo a [0-9]
+    trono.putBox(0,9,0,9,0,9);
+    // desativa os voxels na faixa de [x,y,z] pertencendo a [0-9]
+    trono.cutBox(1,8,1,9,1,9);
+    // grava a escultura digital no arquivo "trono.off"
+    trono.writeOFF("trono.off");
     return 0;
 }
